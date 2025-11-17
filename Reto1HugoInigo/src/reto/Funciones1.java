77fd85895a1057526640897883737fda12434553
HugoMoura88 <1DAM@A11-PC16>
2025-11-17 12:04:56 +0100

    Merge branch 'main' of https://github.com/HugoMoura88/Reto1HugoInigo.git into main

--- Reto1HugoInigo/src/reto/Funciones1.java
+++ Reto1HugoInigo/src/reto/Funciones1.java
@@ -1,5 +1,9 @@
 package reto;
 
 public class Funciones1 {
+	public static void name() {
+		
+	}
+	
 
 }
--- Reto1HugoInigo/src/reto/Reto1.java
+++ Reto1HugoInigo/src/reto/Reto1.java
@@ -1,9 +1,34 @@
 package reto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Reto1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		String menu = """ 
+				Elige la operacion:
+				1. Sumar
+				2. Restar
+				3. Multiplicar
+				4. Dividir
+				5. Par
+				6. Impar
+				7. Positivo
+				8. Negativo
+				0. Salir
+				""";
+		System.out.println(menu);
+		String cadena = sc.nextLine();
+		int opcion = Integer.parseInt(cadena);
+				
+				
+				
+		
+		
+		
+		
 
 	}
 
