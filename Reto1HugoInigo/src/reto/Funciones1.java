197445117d9982c910d4f06ce1cb368102958eaa
HugoMoura88 <1DAM@A11-PC16>
2025-11-17 12:11:08 +0100

    asd

--- Reto1HugoInigo/src/reto/Funciones1.java
+++ Reto1HugoInigo/src/reto/Funciones1.java
@@ -3,6 +3,11 @@ package reto;
 public class Funciones1 {
 	public static void name() {
 		
+		
+		
+		
+		
+		
 	}
 	
 
--- Reto1HugoInigo/src/reto/Funciones2.java
+++ Reto1HugoInigo/src/reto/Funciones2.java
@@ -2,4 +2,17 @@ package reto;
 
 public class Funciones2 {
 
+	public static int multiplicar(int nu1, int nu2) {
+		return nu1*nu2;
+	}
+	public static int dividir(int nu1, int nu2) {
+		return nu1/nu2;
+	}
+	public static boolean esPositivo(int nu1) {
+		return (nu1>=0);
+	}
+	public static boolean esNegativo(int nu1) {
+		return (nu1<0);
+	}
+	
 }
