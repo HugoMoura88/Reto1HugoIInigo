b5f1246545ff84b5aaf2f556def928465e531ec0
1DAM <1DAM@A11-PC15>
2025-11-17 12:15:45 +0100

    klp

--- Reto1HugoInigo/src/reto/Funciones1.java
+++ Reto1HugoInigo/src/reto/Funciones1.java
@@ -1,14 +1,25 @@
 package reto;
 
 public class Funciones1 {
-	public static void name() {
-		
-		
-		
-		
-		
-		
+ 
+	
+	public static int sumar(int n1, int n2) {
+		return n1+n2;
 	}
 	
-
+	public static int restar(int n1,int n2) {
+		return n1-n2;
+	}
+	
+	public static boolean esPar(int n) {
+		if (n%2== 0 ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static void name() {
+		
+	}
 }
